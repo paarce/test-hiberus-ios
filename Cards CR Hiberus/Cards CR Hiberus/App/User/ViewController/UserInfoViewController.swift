81ac853a45128c814acc85448abb3255e89cf45f
Augusto Cordero <augusto100295@gmail.com>
2019-10-28 22:19:02 -0400

    structute and first UI

---
+++ Cards CR Hiberus/Cards CR Hiberus/App/User/ViewController/UserInfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UserInfoViewController.swift
+//  Cards CR Hiberus
+//
+//  Created by Augusto C.P. on 10/28/19.
+//  Copyright © 2019 Augusto C.P. All rights reserved.
+//
+
+import UIKit
+
+class UserInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Cards CR Hiberus/Cards CR Hiberus/App/User/ViewModel/UserInfoViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  UserInfoViewModel.swift
+//  Cards CR Hiberus
+//
+//  Created by Augusto C.P. on 10/28/19.
+//  Copyright © 2019 Augusto C.P. All rights reserved.
+//
+
+import Foundation
